0f4712c2a311bd9f4bf782b5d8f7a525afb0ef51
riyao.jin <jinriyao@qq.com>
2024-08-18 12:31:46 +0800

    使用默认django数据库省去配置的步骤

---
+++ django_nimbot/set_environ_variables.sh
@@ -0,0 +1,8 @@
+#i/usr/bin/env sh
+
+export NVIDIA_API_KEY=你的NV-NIM-key
+#export MYSQL_DATABASE_NAME=数据库名
+#export MYSQL_HOST=数据库ip
+#export MYSQL_PORT=数据库端口
+#export MYSQL_USER=数据库用户名
+#export MYSQL_PASSWORD=数据库密码
